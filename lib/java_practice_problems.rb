5096dd96dc71e5c6964e64f2f2a4a5e23b208de8
prisonmike7 <mwharris7@gmail.com>
2020-06-17 23:14:38 -0700

    First Commit

---
+++ lib/java_practice_problems.rb
@@ -0,0 +1,6 @@
+require "java_practice_problems/version"
+
+module JavaPracticeProblems
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/java_practice_problems/version.rb
@@ -0,0 +1,3 @@
+module JavaPracticeProblems
+  VERSION = "0.1.0"
+end
